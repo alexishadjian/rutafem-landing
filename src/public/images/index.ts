21224566c883a87b0314d4d98a1592a95818b83e
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-12-19 14:22:08 +0100

    feat: add functioning section

--- src/public/images/index.ts
+++ src/public/images/index.ts
@@ -5,6 +5,7 @@ import Value3 from "./value-3.png";
 import Advantage1 from "./advantage-1.png";
 import Advantage2 from "./advantage-2.png";
 import Feature1 from "./feature-1.png";
+import Functioning from "./functioning.png";
 
 
 export {
@@ -14,5 +15,6 @@ export {
     Value3,
     Advantage1,
     Advantage2,
-    Feature1
+    Feature1,
+    Functioning
 }
