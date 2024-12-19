1dc123afc5598d7bb750913b7d661781454facbd
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-12-19 09:01:03 +0100

    feat: add about content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,15 @@ export default function Home() {
                 <button>Inscrivez-vous</button>
             </section>
 
+            <section id="about" className="flex">
+                <div>
+                    <h2>Nous connaitre</h2>
+                </div>
+                <div>
+                    <p>Chez RutaFem, nous avons imaginé une solution simple et rassurante : une plateforme de covoiturage 100 % féminine. Partagez vos trajets, vos frais et bien plus encore, dans un espace pensé pour votre sécurité et votre sérénité. Ensemble, avançons vers une mobilité plus sûre, plus solidaire et plus conviviale.Avec RutaFem, chaque trajet devient une opportunité de rencontre et de partage, dans un cadre bienveillant et respectueux. Nous allions technologie et communauté pour créer une expérience de voyage unique, où chaque femme peut se sentir confiante et soutenue. Parce que voyager ensemble, c'est aussi construire un avenir plus éco-responsable et inclusif.</p>
+                </div>
+            </section>
+
             <EmailForm />
         </div>
     );
