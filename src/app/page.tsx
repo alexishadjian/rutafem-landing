a15951a18c8fdb45acb49d4346722fae4c649aff
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-12-19 11:50:25 +0100

    style: add value style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import EmailForm from "@/components/email-form";
-import { HeroBanner, Feature1, Feature2, Feature3, Advantage1 } from "@/images";
+import { HeroBanner, Feature1, Value1, Value2, Value3, Advantage1 } from "@/images";
 
 
 export default function Home() {
@@ -32,10 +32,10 @@ export default function Home() {
                 </div>
             </section>
 
-            <section id="features" className="flex gap-6 wrapper">
+            <section id="values" className="flex gap-6 wrapper">
                 <div className="relative aspect-[6/7] my-14 text-white p-8 flex justify-end flex-col rounded-3xl overflow-hidden">
                     <Image 
-                        src={Feature1}
+                        src={Value1}
                         quality={100}
                         fill={true}
                         sizes="100%"
@@ -48,7 +48,7 @@ export default function Home() {
                 </div>
                 <div className="relative aspect-[6/7] w-4/6	 text-white p-8 flex justify-end flex-col rounded-3xl overflow-hidden">
                     <Image 
-                        src={Feature2}
+                        src={Value2}
                         quality={100}
                         fill={true}
                         sizes="100%"
@@ -61,7 +61,7 @@ export default function Home() {
                 </div>
                 <div className="relative aspect-[6/7] my-14 text-white p-8 flex justify-end flex-col rounded-3xl overflow-hidden">
                     <Image 
-                        src={Feature3}
+                        src={Value3}
                         quality={100}
                         fill={true}
                         sizes="100%"
@@ -76,7 +76,7 @@ export default function Home() {
 
             <section id="advantage" className="wrapper flex gap-20">
                 <div className="w-1/2">
-                    <h2 className="font-staatliches text-center mb-10">Safe & Simple</h2>
+                    <h2 className="font-staatliches text-center mb-10 text-[--black-taupe]">Safe & Simple</h2>
                     <p className="mb-3">Parce que votre sérénité est notre priorité, nous avons pensé chaque détail pour que vous puissiez voyager l'esprit tranquille. </p>
                     <p className="mb-3">Imaginez partir en covoiturage en toute confiance, en sachant que chaque personne inscrite sur la plateforme a un profil vérifié.</p>
                     <p className="mb-3">Pendant votre trajet, restez connectée : partagez votre localisation en temps réel avec vos proches pour qu'ils sachent exactement où vous êtes. Et si jamais un imprévu survient, notre bouton d’urgence est là, accessible en un instant, pour alerter vos contacts ou nos équipes.</p>
@@ -91,6 +91,52 @@ export default function Home() {
                         alt="home cover"
                         className="z-[-1]"
                     />
+                </div>
+            </section>
+
+            <section id="values" className="wrapper flex gap-10 text-center">
+                <div>
+                    <div className="relative h-[150px] mb-5">
+                        <Image 
+                            src={Feature1}
+                            quality={100}
+                            fill={true}
+                            sizes="100%"
+                            style={{objectFit:"contain"}}
+                            alt="home cover"
+                        />
+                    </div>
+
+                    <h3 className="mb-2">Profil verifié</h3>
+                    <p>Vérification des profils avec carte d'identité et face Id.</p>
+                </div>
+                <div>
+                    <div className="relative h-[150px] mb-5">
+                        <Image 
+                            src={Feature1}
+                            quality={100}
+                            fill={true}
+                            sizes="100%"
+                            style={{objectFit:"contain"}}
+                            alt="home cover"
+                        />
+                    </div>
+                    <h3 className="mb-2">Traçage de la voiture</h3>
+                    <p>Partage de localisation en temps réel avec vos proches.</p>
+                </div>
+                <div>
+                    <div className="relative h-[150px] mb-5">
+                        <Image 
+                            src={Feature1}
+                            quality={100}
+                            fill={true}
+                            sizes="100%"
+                            style={{objectFit:"contain"}}
+                            alt="home cover"
+                        />
+                    </div>
+                    <h3 className="mb-2">Service appel d'urgence</h3>
+                    <p>Un clic suffit pour alerter en cas de besoin.</p>
                 </div>
             </section>
 
--- src/public/images/index.ts
+++ src/public/images/index.ts
@@ -1,14 +1,16 @@
 import HeroBanner from "./hero-banner.png";
-import Feature1 from "./feature-1.png";
-import Feature2 from "./feature-2.png";
-import Feature3 from "./feature-3.png";
+import Value1 from "./value-1.png";
+import Value2 from "./value-2.png";
+import Value3 from "./value-3.png";
 import Advantage1 from "./advantage-1.png";
+import Feature1 from "./feature-1.png";
 
 
 export {
     HeroBanner,
-    Feature1,
-    Feature2,
-    Feature3,
+    Value1,
+    Value2,
+    Value3,
     Advantage1,
+    Feature1
 }
